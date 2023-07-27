adacfbdec31aae636432f9557e7ccbe88fa96937
Wassim Kassem <95465820+wgkassem@users.noreply.github.com>
2023-07-28 08:52:41 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,6 +39,6 @@ PR_TITLE=$(git log -1 --format="%s" ${GITHUB_SHA})
 git_cmd git remote update
 git_cmd git fetch --all
 git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
-git_cmd git cherry-pick "${GITHUB_SHA}"
+git_cmd git cherry-pick --strategy-option "${INPUT_STRATEGY_OPTION}" "${GITHUB_SHA}"
 git_cmd git push -u origin "${PR_BRANCH}"
 git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO: ${PR_TITLE}\""
